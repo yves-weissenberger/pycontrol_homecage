416715b76e71857f092b55d8fde2bc5c7d3ca213
yves <yvesweissenberger@gmail.com>
2021-03-17 13:25:14 +0000

    updated hardware definition

--- config/hardware_definition_9_poke_boxes.py
+++ config/hardware_definition_9_poke_boxes.py
@@ -2,6 +2,7 @@
 # port 4 of breakout board version 1.2.  The houselight is plugged into the center pokes solenoid socket.
 
 from devices import *
+from pyControl.hardware import Digital_output
 
 board = Breakout_1_2()
 
@@ -20,10 +21,15 @@ poke_7 = nine_poke.poke_7
 poke_8 = nine_poke.poke_8
 poke_9 = nine_poke.poke_9
 
+poke_1.SOL = nine_poke.SOL_5
+poke_2.SOL = nine_poke.SOL_3
+poke_3.SOL = nine_poke.SOL_7
 poke_4.SOL = nine_poke.SOL_1
-poke_1.SOL = nine_poke.SOL_4
-poke_9.SOL = nine_poke.SOL_5
+poke_5.SOL =  Digital_output(board.port_6.POW_B)
 poke_6.SOL = nine_poke.SOL_8
+poke_7.SOL = nine_poke.SOL_2
+poke_8.SOL = nine_poke.SOL_6
+poke_9.SOL = nine_poke.SOL_4
 
 houselight = Digital_output(board.port_6.POW_A)
 rsync = Rsync(board.port_6.DIO_B,mean_IPI=5000,pulse_dur=50)
